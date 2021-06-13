7a9947d7f5d26d6c2df5b17b38ead06ed67bcd80
LincolnW90 <83113735+LincolnW90@users.noreply.github.com>
2021-06-13 15:48:18 -0400

    MVP

--- game.js
+++ game.js
@@ -1,6 +1,48 @@
 // create the fight function
+var playername = prompt("What is your robot's name?");
+console.log("Your robot's name is " + playername);
+
+var playerHealth = 100;
+var playerAttack = 10;
+console.log(playername + ": ", "Health remaining " + playerHealth, "Attack power " + playerAttack);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+
 
 function fight(){
-    window.alert("The fight has begun!")
+    // round start alert
+    window.alert("Welcome to Robot Gladiators!")
+
+    // Subtract 'playerAttack' from 'enemyHealth'
+    enemyHealth = enemyHealth - playerAttack;
+    // Log message of new value
+    console.log(playername + "attacked " + enemyName + ". " + enemyName + " has " + enemyHealth + " health remaining.");
+    // check enemy health
+    if (enemyHealth <= 0) {
+        alert(enemyName + " is defeated!!");
+    } else {
+        alert(enemyName + " still has " + enemyHealth + " remaining.");
+    }
+    
+    // Subtract 'enemyAttack' from 'playerHealth
+    playerHealth = playerHealth - enemyAttack;
+    // Log new playerHealth
+    console.log(enemyName + " attacked " + playername + ". " + playername + " has " + playerHealth + " health remaining.");
+    // check player health
+    if (playerHealth <= 0) {
+        alert(playername + " was defeated!!");
+    } else {
+        alert(playername + " still has " + playerHealth + " remaining.");
+    }
+
+
+
+
 }
+
+fight();
+
 
